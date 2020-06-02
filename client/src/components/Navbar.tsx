806fc59f6ca14c2beeb0189713403646b23a64d4
Joshua Pack <joshuapack1@gmail.com>
2020-06-01 20:26:48 -0400

    configured Bulma

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface NavbarProps {
+  navbarLink1: string;
+}
+
+export const Navbar: React.FC<NavbarProps> = (NavbarProps) => {
+  return (
+    <div>
+      <p>sup</p>
+    </div>
+  );
+};
