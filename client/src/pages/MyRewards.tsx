08ea962b12325d27fc1b4fe274e8ab55c3af2b3a
Joshua Pack <joshuapack1@gmail.com>
2020-06-02 21:37:29 -0400

    columns and lists ready for api call data

--- client/src/pages/MyRewards.tsx
+++ client/src/pages/MyRewards.tsx
@@ -1,23 +1,38 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { RewardCard } from "../components/RewardCard";
 import { UserCardsList } from "../components/UserCardsList";
+import { RewardsList } from "../components/RewardsList";
 
 export const MyRewards: React.FC<{}> = (props) => {
   const [userCards, setUserCards] = useState([""]);
-  const sampleUserCards = ["Chase Sapphire Reserve", "Bank of America", "Visa"];
+  const [rewards, setRewards] = useState([""]);
 
   useEffect(() => {
+    const sampleRewards = [
+      "2% cash back at Walmart",
+      "3x bonus points at Walmart",
+    ];
+
+    const sampleUserCards = [
+      "Chase Sapphire Reserve",
+      "Bank of America",
+      "Visa",
+    ];
     setUserCards(sampleUserCards);
-  }, [sampleUserCards]);
+    setRewards(sampleRewards);
+  }, []);
 
   return (
     <Fragment>
-      <div className="columns is-centered">
-        <div className="column has-background-black is-one-third">
+      <div
+        className="columns has-background-white is-centered"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="column is-one-third">
           <UserCardsList userCards={userCards} />
         </div>
-        <div className="column has-background-primary is-two-thirds">
-          Second column
+        <div className="column has-background-white is-two-thirds">
+          <RewardsList rewards={rewards} />
         </div>
       </div>
       {/*<RewardCard cardName="Chase Sapphire" reward="3% back at Walmart" />
